145f81967dd8dd601da4e4070476629b4ad59843
barab. <>
2025-04-16 13:15:00 +0200

    Migrate to flake configuration

---
+++ nixos/global.nix
@@ -0,0 +1,133 @@
+{ config, lib, pkgs, ... }:
+{
+imports = [];
+
+  services.avahi = { # mDNS, IPv4
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+boot.loader.systemd-boot.enable = true; # Systemd boot
+boot.loader.efi.canTouchEfiVariables = true;
+
+networking.networkmanager.enable = true;  # Networkmanager for networking
+
+time.timeZone = "Europe/Paris"; # Time zone
+
+i18n.defaultLocale = "fr_FR.UTF-8"; # Internationalisation properties.
+console = {
+   font = "Lat2-Terminus16";
+   keyMap = "fr";
+};
+
+nix.settings.experimental-features = [ "nix-command" "flakes" ]; # Enable flakes and New ClI
+services.printing.enable = true; # CUPS to manage printers
+services.pipewire = { # Sound manager
+   enable = true;
+   pulse.enable = true;
+ };
+
+# Define a user account. Don't forget to set a password with ‘passwd’.
+users.users.barab = {
+   isNormalUser = true;
+   home = "/home/barab";
+   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+ };
+
+services.greetd = {
+  enable = true;
+  settings = {
+    default_session = {
+        command = "Hyprland --config /etc/greetd/hyprland.conf";
+      };
+  };
+};
+
+services.flatpak.enable = true;
+services.cloudflare-warp.enable = true;
+services.gvfs.enable = true; # For nautilus
+services.upower.enable = true; # For Astal battery
+programs.git = {
+  enable = true;
+};
+programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      aws.disabled = true;
+      gcloud.disabled = true;
+    };
+  };
+programs.bash = {
+    completion.enable = true;
+    shellAliases = { # Shell aliases
+      zed = "zeditor";
+      brice = "zeditor brice && exit";
+      rebuild = "sudo nixos-rebuild switch --flake /home/barab/brice/";
+    };
+  };
+programs.firefox.enable = true;
+programs.hyprland.enable = true;
+programs.kdeconnect.enable = true;
+programs.neovim.enable = true;
+
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+  environment.systemPackages = with pkgs; [
+    kitty # Terminal
+    nautilus # File manager
+    fastfetch
+    walker # Launcher
+    hyprpaper # Wallpaper utility
+    hyprpolkitagent
+    zed-editor
+    bun
+    gh
+    nixd # Nix language server
+    nil
+    brightnessctl
+    grim # Screenshots
+    gnome-themes-extra
+    bibata-cursors
+    greetd.regreet # reGreet
+    gnome-disk-utility
+    btop # Ressource monitor
+    nwg-look # GTK theme editor
+    deskflow
+];
+fonts.packages = with pkgs; [
+  noto-fonts
+  noto-fonts-emoji
+  fira-code
+  fira-code-symbols
+];
+# Allowing unfree packages
+nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "cloudflare-warp"
+];
+# Définir le curseur globalement
+environment.variables = {
+  XCURSOR_THEME = "Bibata-Modern-Classic";
+  XCURSOR_SIZE = "16";
+  HYPRLAND_CONFIG = "brice/config/hypr/hyprland.conf";
+  EDITOR="zeditor --wait";
+};
+
+environment.sessionVariables.NIXOS_OZONE_WL = "1"; # Hint Electron apps to use Wayland
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 4321 ];
+  };
+}
---
+++ nixos/hosts/nix-portable/default.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, ... }:
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../global.nix
+    ];
+  programs.bash.shellAliases = {
+    wifi = "nmtui";
+  };
+  services.libinput.enable = true; # Enable touchpad support (enabled default in most desktopManager).
+  hardware.bluetooth.enable = true; # Enable support for Bluetooth
+  services.blueman.enable = true; # GUI Bluetooth manager
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [];
+  # Allowing unfree packages
+  #nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [];
+  # Définir le curseur globalement
+  #environment.variables = {};
+  #networking.firewall = {
+    # allowedTCPPorts = [ 4321 ];
+    #};
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true; # not supported by flakes
+
+  # This option defines the firEDITOR="zeditor --wait"st version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.11"; # Did you read the comment?
+}
