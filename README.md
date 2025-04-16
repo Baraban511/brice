9c1891648ccf909c841443be4fbeb02e0f6607b1
barab. <>
2025-04-16 13:15:00 +0200

    Migrate to flake configuration

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ My Nixos configuration and ricing. It's a work in progress.
 
 ## Installation
 Run ```cd brice && sh ./install.sh``` to install the configuration.
+```sudo nixos-rebuild switch --flake /home/barab/brice/```
--- install.sh
+++
@@ -1,4 +0,0 @@
-sudo ln -s ~/brice/config/nixos /etc/ # Find way to keep Hardware but link config
-sudo rm -r ~/.config/hypr
-sudo ln -s ~/brice/config/hypr ~/.config/
-echo 'eval "$(starship init bash)"' >> ~/.bashrc # Add starship to bash
