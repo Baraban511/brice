3c55cc0272760edb32a84e160b7e3a1c83334f88
barab. <>
2025-04-16 13:15:00 +0200

    Migrate to flake ocnfiguration

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "brice, barab.'s nixos configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations= {
+      nix-pc = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/hosts/nix-pc
+          ({ config, pkgs, ... }: {
+                  environment.etc."greetd/hyprland.conf".source = "${self}/config/greetd/hyprland.conf";
+                  environment.etc."gtk-3.0/settings.ini".source = "${self}/config/gtk-3.0.ini";
+                  environment.etc."greetd/regreet.toml".source = "${self}/config/greetd/regreet.toml";
+                  environment.etc."greetd/regreet.png".source = "${self}/wallpapers/regreet.png";
+                })
+        ];
+      };
+
+      nix-portable = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/hosts/nix-pc/nix-portable
+          ({ config, pkgs, ... }: {
+                  environment.etc."greetd/hyprland.conf".source = "${self}/config/greetd/hyprland.conf";
+                  environment.etc."gtk-3.0/settings.ini".source = "${self}/config/gtk-3.0.ini";
+                  environment.etc."greetd/regreet.toml".source = "${self}/config/greetd/regreet.toml";
+                  environment.etc."greetd/regreet.png".source = "${self}/wallpapers/regreet.png";
+                })
+        ];
+      };
+    };
+  };
+}
